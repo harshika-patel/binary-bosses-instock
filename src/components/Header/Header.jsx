4068f759762566fb7d487eeea075324dfe2e7251
Harshika Patel <harshikapatel2574@gmail.com>
2025-02-04 14:02:42 -0500

    Add Header Style

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,17 @@
-import logo from '../../assets/Logo/inStock-Logo_1x.png';
+import './Header.scss'
+import logo from '../../assets/Logo/InStock-Logo.svg'
 const Header=(props)=>{
     return(
         <header className='header'>
             <div className='header__logo'>
-            <img src={logo} alt="instock_logo" className='header__logo-img' />
-            <h1 className='header__logo-title'>INSTOCK</h1>
+                <img src={logo} alt="instock_logo" className='header__logo-img' />
+               
             </div>
             <div className='header__nav'>
-              <h3 className='header__nav-warehouse'>Warehouses</h3>
-              <h3 className='header__nav-warehouse'>Inventory</h3>
+            <button className='header__btn'> <h3 className='header__nav-warehouse'>Warehouses</h3></button> 
+              <button  className='header__btn'><h3 className='header__nav-warehouse'>Inventory</h3></button> 
             </div>
         </header>
     )
 }
-export default Header;
+export default Header; 
