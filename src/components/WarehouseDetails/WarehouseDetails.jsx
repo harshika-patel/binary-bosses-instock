e503ede539fd25fc40142d4b1d16ca60bddda944
olivianoell <oliviacatherinenoell@gmail.com>
2025-02-05 12:25:42 -0500

    styling added

--- src/components/WarehouseDetails/WarehouseDetails.jsx
+++ src/components/WarehouseDetails/WarehouseDetails.jsx
@@ -26,38 +26,38 @@ const WarehouseDetails = () => {
 
     if (!warehouse) return <p>Loading...</p>
 
-
-
     return (
-    <section>
-        <div>
-            <img src={arrowBack} alt="back" />
-            <h2>{warehouse.warehouse_name}</h2>
-            <img src={editWhiteFill} alt="edit" />
-        </div>
-
-        <hr></hr>
-        <div>
-            <div>
-                <p>WAREHOUSE ADDRESS:</p>
-                <p>{warehouse.address}</p>
+        <section>
+            <div className="title-wrapper">
+                <div className="title-wrapper__left">
+                    <img src={arrowBack} alt="back" />
+                    <h2>{warehouse.warehouse_name}</h2>
+                </div>
+                <img className="title-wrapper__right" src={editWhiteFill} alt="edit" />
             </div>
-            <div>
-                <p>CONTANCT NAME:</p>
-                <p>{warehouse.contact_name}</p>
-                <p>{warehouse.contact_position}</p>
-            </div>
-            <div>
-                <p>CONTACT INFORMATION</p>
-                <p>{warehouse.contact_phone}</p>
-                <p>{warehouse.contact_email}</p>
-            </div>
-        </div>
 
-        <hr></hr>
-        
+            <hr></hr>
+            <div className="details">
+                <div>
+                    <p className="details__labels">WAREHOUSE ADDRESS:</p>
+                    <p className="details__outputs">{warehouse.address}</p>
+                </div>
+                <div className="details__contact-section">
+                    <div>
+                        <p className="details__labels">CONTANCT NAME:</p>
+                        <p className="details__outputs">{warehouse.contact_name}</p>
+                        <p className="details__outputs">{warehouse.contact_position}</p>
+                    </div>
+                    <div>
+                        <p>CONTACT INFORMATION:</p>
+                        <p className="details__labels">{warehouse.contact_phone}</p>
+                        <p className="details__outputs">Phone</p>
+                        <p className="details__outputs">{warehouse.contact_email}</p>
+                    </div>
+                </div>
+            </div>        
 
-    </section>
+        </section>
     );
 };
 
