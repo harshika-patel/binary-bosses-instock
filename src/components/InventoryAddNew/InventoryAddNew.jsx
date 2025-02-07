e7cce931a9c54958ad08630b27bd81d9ed9e00ed
Cecilia Leung <Cecilialeung05@gmail.com>
2025-02-06 19:49:41 -0500

    fix structure, dropdown and radio button

--- src/components/InventoryAddNew/InventoryAddNew.jsx
+++ src/components/InventoryAddNew/InventoryAddNew.jsx
@@ -1,8 +1,8 @@
 import "./InventoryAddNew.scss";
 import React, { useState } from "react";
-import axios from "axios"; // Import Axios
-import backAerrow from "../../assets/Icons/arrow_back-24px.svg";
-import { Link} from "react-router-dom"; // Use history for navigation
+import axios from "axios"; 
+import backArrow from "../../assets/Icons/arrow_back-24px.svg";
+import { Link} from "react-router-dom"; 
 
 const InventoryAddNew = () => {
   
@@ -18,14 +18,14 @@ const InventoryAddNew = () => {
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
-    setErrors({ ...errors, [e.target.name]: "" }); // Clear error on field change
+    setErrors({ ...errors, [e.target.name]: "" }); 
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     let newErrors = {};
 
-    // Validate fields manually
+
     if (!formData.warehouse_id || !formData.item_name||!formData.description||!formData.category||!formData.status||(formData.status === "In Stock" && !formData.quantity)) {
       newErrors.warehouse_id = 'All fields are required.';
     }
@@ -48,7 +48,7 @@ const InventoryAddNew = () => {
         setError("Quantity must be a valid number.");
         return;
       }
-    // If there are any errors, update the state and stop form submission
+
     if (Object.keys(newErrors).length > 0) {
       setErrors(newErrors);
       return;
@@ -87,8 +87,8 @@ const InventoryAddNew = () => {
   };
 
   const handleCancel = () => {
-    // Optionally navigate away or reset form
-    history.push("/inventory-details"); // Navigate back to warehouse details page
+
+    history.push("/inventory-details");
   };
 
   return (
@@ -97,82 +97,111 @@ const InventoryAddNew = () => {
         <Link to="/inventory-details" className="inventory-form__header-link">
             <img src={backArrow} alt="Back" className="back-arrow" />
         </Link>
-        <h1>Add New Inventory</h1>
+        <h1>Add New Inventory Item</h1>
       </div>
       <hr />
       <form onSubmit={handleSubmit}>
         <div className="section">
-          <h2>Inventory Details</h2>
-          <label><h3>Inventory ID</h3></label>
-          <input
-            type="number"
-            name="warehouseId"
-            value={formData.warehouse_id}
-            onChange={handleChange}
-            placeholder="Warehouse ID"
-            className={errors.warehouse_id ? "error" : ""}
-          />
-          {errors.warehouse_id && <span className="error-message">{errors.warehouse_id}</span>}
-
-          <label><h3>Item Name</h3></label>
-          <input
+            <h2>Item Details</h2>
+            <label><h3>Item Name</h3></label>
+            <input
             type="text"
             name="itemName"
             value={formData.item_name}
             onChange={handleChange}
             placeholder="Item Name"
             className={errors.item_name ? "error" : ""}
-          />
-          {errors.item_name && <span className="error-message">{errors.itemName}</span>}
+            />
+            {errors.item_name && <span className="error-message">{errors.itemName}</span>}
 
-          <label><h3>Description</h3></label>
-          <textarea
+            <label><h3>Description</h3></label>
+            <textarea
             type="text"
             name="description"
             value={formData.description}
             onChange={handleChange}
             placeholder="Description"
             className={errors.description ? "error" : ""}
-          />
-          {errors.description && <span className="error-message">{errors.description}</span>}
+            />
+            {errors.description && <span className="error-message">{errors.description}</span>}
 
-          <label><h3>Category</h3></label>
-          <input
-            type="text"
-            name="category"
-            value={formData.category}
-            onChange={handleChange}
-            placeholder="Category"
-            className={errors.category ? "error" : ""}
-          />
-          {errors.category && <span className="error-message">{errors.category}</span>}
+            <label><h3>Category</h3></label>
+            <select
+                name="category"
+                value={formData.category}
+                onChange={handleChange}
+                className={errors.category ? "dropdown error" : "dropdown"}
+            >
+                <option value="">Please select</option>
+                <option value="Electronics">Electronics</option>
+                <option value="Furniture">Furniture</option>
+                <option value="Clothing">Clothing</option>
+                <option value="Food">Food</option>
+            </select>
+        {errors.category && <span className="error-message">{errors.category}</span>}
         </div>
 
         <hr />
         {/* Status*/}
         <div className="section">
-        <h2>Status:</h2>
+            <h2>Item Availability</h2>
         <label><h3>Status</h3></label>
-        <select name="status" value={formData.status} onChange={handleChange}>
-        <option value="In Stock">In Stock</option>
-        <option value="Out of Stock">Out of Stock</option>
-        </select>
+        <div className="status-options">
+        <label>
+            <input 
+                type="radio" 
+                name="status" 
+                value="In Stock" 
+                checked={formData.status === "In Stock"}
+                onChange={handleChange}
+            />
+            In Stock
+        </label>
+        
+        <label>
+            <input 
+                type="radio" 
+                name="status" 
+                value="Out of Stock" 
+                checked={formData.status === "Out of Stock"}
+                onChange={handleChange}
+            />
+            Out of Stock
+        </label>
+        </div>
 
-        {formData.status === "In Stock" && (
-        <>
-          <label>Quantity:</label>
-          <input
-            type="number"
-            name="quantity"
-            value={formData.quantity}
+    {formData.status === "In Stock" && (
+        <div className="quantity-field">
+            <label>Quantity:</label>
+            <input
+                type="number"
+                name="quantity"
+                value={formData.quantity}
+                onChange={handleChange}
+                min="1"
+                placeholder="Enter quantity"
+            />
+        </div>
+    )}
+
+        <label><h3>Warehouse</h3></label>
+        <select
+            name="warehouse"
+            value={formData.warehouseId}
             onChange={handleChange}
-          />
-        </>
-        )}
-
+            className={errors.warehouse ? "dropdown error" : "dropdown"}
+        >
+            <option value="">Please select</option>
+            <option value="Warehouse A">Warehouse A</option>
+            <option value="Warehouse B">Warehouse B</option>
+            <option value="Warehouse C">Warehouse C</option>
+            <option value="Warehouse D">Warehouse D</option>
+        </select>
+        {errors.warehouse && <span className="error-message">{errors.warehouse}</span>}
+        </div>
         <div className="buttons">
         <Link to="/inventory-details"><button type="button" className="cancel" onClick={handleCancel}>Cancel</button></Link>
-        <Link to="/inventory-details"><button type="save" className="save">Save</button></Link> 
+        <Link to="/inventory-details"><button type="submit" className="add-inventoryitem">+ Add Warehouse</button></Link> 
         </div>
       </form>
     </div>
