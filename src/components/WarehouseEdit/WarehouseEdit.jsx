8230e9327fe995dfc0ad0a08a67c7d9894c3b7b4
Elizaveta Radaeva <RADAEVA.LIZA98@GMAIL.COM>
2025-02-07 21:05:48 -0500

    style changes, still a couple minor tweaks needed, functionality works

--- src/components/WarehouseEdit/WarehouseEdit.jsx
+++ src/components/WarehouseEdit/WarehouseEdit.jsx
@@ -1,4 +1,3 @@
-
 import "./WarehouseEdit.scss";
 import React, { useState } from "react";
 import { Link, useNavigate, useParams } from "react-router-dom";
@@ -39,7 +38,8 @@ const WarehouseEdit = () => {
       setMessage("Warehouse updated successfully!");
       navigate(`/warehouse-details/${warehouseId}`);
     } catch (error) {
-      const errorMsg = error.response?.data?.message || "Error updating warehouse.";
+      const errorMsg =
+        error.response?.data?.message || "Error updating warehouse.";
       setMessage(errorMsg);
       console.error(error.response?.data || error.message);
     } finally {
@@ -57,48 +57,121 @@ const WarehouseEdit = () => {
       </div>
       <hr className="warehouse-edit__divider" />
       <form onSubmit={handleSubmit} className="warehouse-edit__form">
-        <div className="form-section">
+        <article className="form-section">
           <div className="section">
             <h2>Warehouse Details</h2>
-            <label htmlFor="warehouse_name"><h3>Warehouse Name</h3></label>
-            <input id="warehouse_name" type="text" name="warehouse_name" value={formData.warehouse_name} onChange={handleChange} />
+            <label htmlFor="warehouse_name">
+              <h3>Warehouse Name</h3>
+            </label>
+            <input
+              id="warehouse_name"
+              type="text"
+              name="warehouse_name"
+              value={formData.warehouse_name}
+              onChange={handleChange}
+            />
 
-            <label htmlFor="address" className="warehouse-edit__label"><h3>Street Address</h3></label>
-            <input id="address" type="text" name="address" className="warehouse-edit__input" value={formData.address} onChange={handleChange} />
+            <label htmlFor="address" className="warehouse-edit__label">
+              <h3>Street Address</h3>
+            </label>
+            <input
+              id="address"
+              type="text"
+              name="address"
+              className="warehouse-edit__input"
+              value={formData.address}
+              onChange={handleChange}
+            />
 
-            <label htmlFor="city" className="warehouse-edit__label"><h3>City</h3></label>
-            <input id="city" type="text" name="city" className="warehouse-edit__input" value={formData.city} onChange={handleChange} />
+            <label htmlFor="city" className="warehouse-edit__label">
+              <h3>City</h3>
+            </label>
+            <input
+              id="city"
+              type="text"
+              name="city"
+              className="warehouse-edit__input"
+              value={formData.city}
+              onChange={handleChange}
+            />
 
-            <label htmlFor="country" className="warehouse-edit__label"><h3>Country</h3></label>
-            <input id="country" type="text" name="country" className="warehouse-edit__input" value={formData.country} onChange={handleChange} />
+            <label htmlFor="country" className="warehouse-edit__label">
+              <h3>Country</h3>
+            </label>
+            <input
+              id="country"
+              type="text"
+              name="country"
+              className="warehouse-edit__input"
+              value={formData.country}
+              onChange={handleChange}
+            />
           </div>
 
           <hr className="warehouse-edit__divider" />
-          
+
           <div className="section">
             <h2>Contact Details</h2>
-            <label htmlFor="contact_name" className="warehouse-edit__label"><h3>Contact Name</h3></label>
-            <input id="contact_name" type="text" name="contact_name" className="warehouse-edit__input" value={formData.contact_name} onChange={handleChange} />
+            <label htmlFor="contact_name" className="warehouse-edit__label">
+              <h3>Contact Name</h3>
+            </label>
+            <input
+              id="contact_name"
+              type="text"
+              name="contact_name"
+              className="warehouse-edit__input"
+              value={formData.contact_name}
+              onChange={handleChange}
+            />
 
-            <label htmlFor="contact_position" className="warehouse-edit__label"><h3>Position</h3></label>
-            <input id="contact_position" type="text" name="contact_position" className="warehouse-edit__input" value={formData.contact_position} onChange={handleChange} />
+            <label htmlFor="contact_position" className="warehouse-edit__label">
+              <h3>Position</h3>
+            </label>
+            <input
+              id="contact_position"
+              type="text"
+              name="contact_position"
+              className="warehouse-edit__input"
+              value={formData.contact_position}
+              onChange={handleChange}
+            />
 
-            <label htmlFor="contact_phone" className="warehouse-edit__label"><h3>Phone Number</h3></label>
-            <input id="contact_phone" type="tel" name="contact_phone" className="warehouse-edit__input" value={formData.contact_phone} onChange={handleChange} />
+            <label htmlFor="contact_phone" className="warehouse-edit__label">
+              <h3>Phone Number</h3>
+            </label>
+            <input
+              id="contact_phone"
+              type="tel"
+              name="contact_phone"
+              className="warehouse-edit__input"
+              value={formData.contact_phone}
+              onChange={handleChange}
+            />
 
-            <label htmlFor="contact_email" className="warehouse-edit__label"><h3>Email</h3></label>
-            <input id="contact_email" type="email" name="contact_email" className="warehouse-edit__input" value={formData.contact_email} onChange={handleChange} />
+            <label htmlFor="contact_email" className="warehouse-edit__label">
+              <h3>Email</h3>
+            </label>
+            <input
+              id="contact_email"
+              type="email"
+              name="contact_email"
+              className="warehouse-edit__input"
+              value={formData.contact_email}
+              onChange={handleChange}
+            />
           </div>
-
-          <div className="buttons">
-            <Link to={`/warehouse-details/${warehouseId}`}>
-              <button type="button" className="cancel">Cancel</button>
-            </Link>
-            <button type="submit" className="cancel save" disabled={loading}>
-              {loading ? "Updating..." : "Save"}
+        </article>
+        <div className="buttons">
+          <Link to={`/warehouse-details/${warehouseId}`}>
+            <button type="button" className="cancel">
+              Cancel
             </button>
-          </div>
+          </Link>
+          <button type="submit" className="save" disabled={loading}>
+            {loading ? "Updating..." : "Save"}
+          </button>
         </div>
+
         {message && <p className="warehouse-edit__message">{message}</p>}
       </form>
     </section>
